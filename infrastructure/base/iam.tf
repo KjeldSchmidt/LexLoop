6fe068f717b61d0d52bbfd14d80f9927597a8da6
Kjeld Schmidt <contact@kjeld-schmidt.com>
2025-10-01 00:25:23 +0200

    fix: allow role assumption via the exact provider we instantiate

--- infrastructure/base/iam.tf
+++ infrastructure/base/iam.tf
@@ -9,7 +9,7 @@ resource "aws_iam_role" "cicd_deploy" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Federated = "arn:aws:iam::${local.account_id}:oidc-provider/token.actions.githubusercontent.com"
+          Federated = aws_iam_openid_connect_provider.github.arn
         }
         Condition = {
           StringEquals = {
@@ -48,7 +48,6 @@ resource "aws_iam_openid_connect_provider" "github" {
   ]
 
   thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1",
-    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
   ]
 }
