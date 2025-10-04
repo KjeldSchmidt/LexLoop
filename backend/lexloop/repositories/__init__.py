ba4784879c69d2b78a112638c2db19adde6a4ef7
Kjeld Schmidt <contact@kjeld-schmidt.com>
2025-10-04 15:36:39 +0200

    chore: try sidestepping permissions error

--- backend/lexloop/repositories/__init__.py
+++ backend/lexloop/repositories/__init__.py
@@ -1,3 +1,8 @@
+import os
+
+
 class MetaBase:
     host = "http://localhost:7894"
     region = "eu-central-1"
+    aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
