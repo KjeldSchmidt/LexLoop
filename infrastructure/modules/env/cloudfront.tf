b73099965b3255c8dc9070bf8d66a8ab493f2455
Kjeld Schmidt <contact@kjeld-schmidt.com>
2025-10-23 00:09:16 +0200

    fix: try to set origin domain without protocol prefix

--- infrastructure/modules/env/cloudfront.tf
+++ infrastructure/modules/env/cloudfront.tf
@@ -2,32 +2,32 @@ resource "aws_cloudfront_distribution" "cdn" {
   enabled             = true
   default_root_object = "index.html"
 
-  # origin {
-  #   domain_name              = aws_s3_bucket.static_site.bucket_domain_name
-  #   origin_id                = "${local.project_slug}-frontend-${var.env}"
-  #   origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
-  # }
+  origin {
+    domain_name              = aws_s3_bucket.static_site.bucket_domain_name
+    origin_id                = "${local.project_slug}-frontend-${var.env}"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
 
   origin {
-    domain_name = aws_apigatewayv2_api.api.api_endpoint
+    domain_name = replace(aws_apigatewayv2_api.api.api_endpoint, "https://", "")
     origin_id   = "${local.project_slug}-api-${var.env}"
     origin_path = "/$default"
   }
 
-  # default_cache_behavior {
-  #   target_origin_id       = "${local.project_slug}-frontend-${var.env}"
-  #   viewer_protocol_policy = "redirect-to-https"
-  #   allowed_methods        = ["GET", "HEAD"]
-  #   cached_methods         = ["GET", "HEAD"]
-  # }
-
-  # ordered_cache_behavior {
   default_cache_behavior {
-    # path_pattern           = "/api/*"
+    target_origin_id       = "${local.project_slug}-frontend-${var.env}"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+  }
+
+  ordered_cache_behavior {
+    path_pattern           = "/api/*"
     target_origin_id       = "${local.project_slug}-api-${var.env}"
     viewer_protocol_policy = "redirect-to-https"
     allowed_methods        = ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE"]
-    cached_methods         = ["GET", "HEAD"]
+    cached_methods         = []
+    cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # Taken from https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html#managed-cache-policy-caching-disabled
   }
 
   viewer_certificate {
