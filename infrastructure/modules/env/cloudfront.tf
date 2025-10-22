b4c54586f3c7f6b35ac318d7c482040e52e95e5f
Kjeld Schmidt <contact@kjeld-schmidt.com>
2025-10-23 00:11:23 +0200

    fix; try deploying only the s3 part

--- infrastructure/modules/env/cloudfront.tf
+++ infrastructure/modules/env/cloudfront.tf
@@ -8,11 +8,11 @@ resource "aws_cloudfront_distribution" "cdn" {
     origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
   }
 
-  origin {
-    domain_name = replace(aws_apigatewayv2_api.api.api_endpoint, "https://", "")
-    origin_id   = "${local.project_slug}-api-${var.env}"
-    origin_path = "/$default"
-  }
+  # origin {
+  #   domain_name = replace(aws_apigatewayv2_api.api.api_endpoint, "https://", "")
+  #   origin_id   = "${local.project_slug}-api-${var.env}"
+  #   origin_path = "/$default"
+  # }
 
   default_cache_behavior {
     target_origin_id       = "${local.project_slug}-frontend-${var.env}"
@@ -21,14 +21,14 @@ resource "aws_cloudfront_distribution" "cdn" {
     cached_methods         = ["GET", "HEAD"]
   }
 
-  ordered_cache_behavior {
-    path_pattern           = "/api/*"
-    target_origin_id       = "${local.project_slug}-api-${var.env}"
-    viewer_protocol_policy = "redirect-to-https"
-    allowed_methods        = ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE"]
-    cached_methods         = []
-    cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # Taken from https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html#managed-cache-policy-caching-disabled
-  }
+  # ordered_cache_behavior {
+  #   path_pattern           = "/api/*"
+  #   target_origin_id       = "${local.project_slug}-api-${var.env}"
+  #   viewer_protocol_policy = "redirect-to-https"
+  #   allowed_methods        = ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE"]
+  #   cached_methods         = []
+  #   cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # Taken from https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html#managed-cache-policy-caching-disabled
+  # }
 
   viewer_certificate {
     cloudfront_default_certificate = true
