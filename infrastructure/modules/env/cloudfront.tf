1e1258fc94cc85fafcc9093fd8bd24dc0c726fa9
Kjeld Schmidt <contact@kjeld-schmidt.com>
2025-10-23 00:16:10 +0200

    fix: set forwarded_values

--- infrastructure/modules/env/cloudfront.tf
+++ infrastructure/modules/env/cloudfront.tf
@@ -19,6 +19,13 @@ resource "aws_cloudfront_distribution" "cdn" {
     viewer_protocol_policy = "redirect-to-https"
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
+    
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
   }
 
   # ordered_cache_behavior {
