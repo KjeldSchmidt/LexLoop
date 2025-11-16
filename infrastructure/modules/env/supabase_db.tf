f5bbae2191dc0cf2c61458a5f5d92e8f33c426e8
Kjeld Schmidt <contact@kjeld-schmidt.com>
2025-11-16 02:42:24 +0100

    reactivate supabase IaC

--- infrastructure/modules/env/supabase_db.tf
+++ infrastructure/modules/env/supabase_db.tf
@@ -1,11 +1,11 @@
-# resource "supabase_project" "_" {
-#   organization_id   = local.supabase_org_id
-#   name              = "${local.project_slug}-db-${var.env}"
-#   database_password = random_password.supabase_password.result
-#   region            = local.region
-# }
-#
-# resource "random_password" "supabase_password" {
-#   length  = 64
-#   special = true
-# }
+resource "supabase_project" "_" {
+  organization_id   = local.supabase_org_id
+  name              = "${local.project_slug}-db-${var.env}"
+  database_password = random_password.supabase_password.result
+  region            = local.region
+}
+
+resource "random_password" "supabase_password" {
+  length  = 64
+  special = true
+}
