3615178fbbcb3b0beca1085e4315357386668a38
Kjeld Schmidt <contact@kjeld-schmidt.com>
2026-01-25 15:05:34 +0100

    feat: send requests on the deployed system to the deployed lambda

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,6 +1,6 @@
 import createClient from 'openapi-fetch'
 import type { components, paths } from '@/api/schema.ts'
-const client = createClient<paths>({ baseUrl: 'http://127.0.0.1:8000/api/' })
+const client = createClient<paths>({ baseUrl: import.meta.env.VITE_API_URL })
 export default client
 export type schemas = components['schemas']
 type NodeIn = schemas['NodeIn']
